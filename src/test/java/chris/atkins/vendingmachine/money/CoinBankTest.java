07b2b611d5e3cfd23482c225bd7e676661bb0d24
Chris Atkins <chrisatkins55@gmail.com>
2015-06-17 09:13:15 -0400

    returns multiple coins if needed

--- src/test/java/chris/atkins/vendingmachine/money/CoinBankTest.java
+++ src/test/java/chris/atkins/vendingmachine/money/CoinBankTest.java
@@ -3,12 +3,13 @@ package chris.atkins.vendingmachine.money;
 import static chris.atkins.vendingmachine.money.Coin.DIME;
 import static chris.atkins.vendingmachine.money.Coin.NICKEL;
 import static chris.atkins.vendingmachine.money.Coin.QUARTER;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import chris.atkins.vendingmachine.CoinReturn;
@@ -26,18 +27,33 @@ public class CoinBankTest {
 	@Test
 	public void returnsANickel() throws Exception {
 		this.bank.returnChange(0.05, this.coinReturn);
-		Mockito.verify(this.coinReturn).returnCoin(NICKEL);
+		verify(this.coinReturn).returnCoin(NICKEL);
 	}
 
 	@Test
 	public void returnsADime() throws Exception {
 		this.bank.returnChange(0.10, this.coinReturn);
-		Mockito.verify(this.coinReturn).returnCoin(DIME);
+		verify(this.coinReturn).returnCoin(DIME);
 	}
 
 	@Test
 	public void returnsAQuarter() throws Exception {
 		this.bank.returnChange(0.25, this.coinReturn);
-		Mockito.verify(this.coinReturn).returnCoin(QUARTER);
+		verify(this.coinReturn).returnCoin(QUARTER);
+	}
+
+	@Test
+	public void returnsMultipleCoins() throws Exception {
+		this.bank.returnChange(0.40, this.coinReturn);
+		verify(this.coinReturn).returnCoin(QUARTER);
+		verify(this.coinReturn).returnCoin(DIME);
+		verify(this.coinReturn).returnCoin(NICKEL);
+	}
+
+	@Test
+	public void returnsBestDenominations() throws Exception {
+		this.bank.returnChange(0.45, this.coinReturn);
+		verify(this.coinReturn).returnCoin(QUARTER);
+		verify(this.coinReturn, times(2)).returnCoin(DIME);
 	}
 }
